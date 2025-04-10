caf895f958abd020f9e33fc998586c01a1e0898b
Florencia Scolari <florscolari0@gmail.com>
2025-04-10 17:21:51 +0800

    added mean, median and population standard deviation

--- TECH6100A1.py
+++ TECH6100A1.py
@@ -1,30 +1,42 @@
 # TECH6100 Assessment 1 Florencia Scolari ID 1847863 Apr 2025
-
-
-
 def calculate_statistics(*numbers):
-    """To calculate the mean, median, and standard deviation of the given values"""
     if len(numbers) > 0:
-        mean = sum(numbers) / len(numbers) #average value
+        """To get the MEAN"""
+        mean = sum(numbers) / len(numbers)  # average value
+
+        """To get the MEDIAN"""
         median = sorted(numbers)
-        """2 cases for when: list is odd or list is even"""
-        m = len(numbers)//2 # m is the middle point. '//' is floor division, rounded to min closest digit
-        if len(numbers)%2!=0: #reminder not equal to 0 = ODD
+        """Calculate Median for 2 cases: list is odd or list is even"""
+        m = len(numbers) // 2  # m is the middle point. '//' is floor division, rounded to min closest digit
+        if len(numbers) % 2 != 0:  # reminder not equal to 0 = ODD
             median = numbers[m]
-        elif len(numbers)%2==0: #reminder equals to 0 = EVEN
-            median = (numbers[m-1]+numbers[m])/2 #Calculating median for even numbers
-        #todo: to be continue on deviation
+        elif len(numbers) % 2 == 0:  # reminder equals to 0 = EVEN
+            median = (numbers[m - 1] + numbers[m]) / 2  # Calculating median for even numbers
 
-        #deviation = stdev(numbers,mean)
-        return float(mean), float(median)
+        """To get the POPULATION STANDARD DEVIATION"""
+        temp = []
+        for item in numbers:
+            item = (item - mean)  # 2. Subtract the mean from each data point.
+            item = item * item  # 3. Square each deviation to make it positive.
+            temp.append(item)  # Hold all squared deviations together (list)
+        total = 0
+        for num in temp:
+            total += num  # 4. Sum of all squared deviations together
+        variance = total / len(numbers)  # 5. Divide the sum by the number of data points (called Variance)
+        deviation = variance ** 0.5  # 6. Get the square root of the variance (called population std deviation)
+
+        return {
+            'mean': float(mean),
+            'median': float(median),
+            'std_dev': round(deviation, 3)
+        }
     else:
-        return None
+        message = "No numbers were given."
+        return message
 
-test1 = calculate_statistics(20, 30, 40)
-test2 = calculate_statistics(3.5, 2.56, 12, 34, 90)
+
 test3 = calculate_statistics(1, 2, 3, 4, 5)
 test4 = calculate_statistics(10, 15, 20, 25, 30, 35)
-print(test1)
-print(test2)
-print(test3)
-print(test4)
+
+print('Test 3', test3)
+print('Test 4', test4)
--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,35 @@
+# TECH6100 Assessment 1 Florencia Scolari ID 1847863 Apr 2025
+def calculate_statistics(*numbers):
+    if len(numbers) > 0:
+        """To get the MEAN"""
+        mean = sum(numbers) / len(numbers) #average value
 
+        """To get the MEDIAN"""
+        median = sorted(numbers)
+        """Calculate Median for 2 cases: list is odd or list is even"""
+        m = len(numbers) // 2  # m is the middle point. '//' is floor division, rounded to min closest digit
+        if len(numbers) % 2 != 0:  # reminder not equal to 0 = ODD
+            median = numbers[m]
+        elif len(numbers) % 2 == 0:  # reminder equals to 0 = EVEN
+            median = (numbers[m - 1] + numbers[m]) / 2  # Calculating median for even numbers
+
+        """To get the POPULATION STANDARD DEVIATION"""
+        temp = []
+        for item in numbers:
+            item = (item - mean) #2. Subtract the mean from each data point.
+            item = item * item #3. Square each deviation to make it positive.
+            temp.append(item) # Hold all squared deviations together (list)
+        total = 0
+        for num in temp:
+            total += num #4. Sum of all squared deviations together
+        variance = total / len(numbers) #5. Divide the sum by the number of data points (called Variance)
+        deviation = variance**0.5 #6. Get the square root of the variance (called population std deviation)
+
+        return float(mean), float(median), round(deviation,3)
+    else:
+        return None
+
+test5 = calculate_statistics(1, 2, 3, 4, 5)
+
+
+print('Testing deviation: ', test5)
