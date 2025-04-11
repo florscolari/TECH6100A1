9cb60bb9f030fc20d85648207d2ca6a782d34203
Florencia Scolari <florscolari0@gmail.com>
2025-04-11 19:07:44 +0800

    added Task 2 on task 2

--- task 2.py
+++ task 2.py
@@ -1,4 +1,12 @@
 ### Task 2 Pseudocode Guidance
+def create_dictionaries(number_of_dict):
+    n = 0
+    while n != number_of_dict:
+        parent_key = input(f"Enter the Key for dictionary #{n+1}: ")
+        parent_value = {}
+        parent_keys_qty[parent_key] = parent_value
+        n += 1
+
 def merge_dictionaries(**kwargs):
     result_dict = {}
     for dict_name, dict_value in kwargs.items():
@@ -17,14 +25,8 @@ def merge_dictionaries(**kwargs):
     print(f"{'-'*10}\nYou have merged {len(kwargs.items())} dictionaries into 1.\nThe result has {len(result_dict.items())} key-value pairs.\n{result_dict}")
 
 
-
+## Running Program with User Inputs
 parent_keys_qty = {}
-number_of_dict = int(input("Enter the number of dictionaries you would like to merge: "))
-n = 0
-while n != number_of_dict:
-    parent_key = input(f"Enter the Key for dictionary #{n+1}: ")
-    parent_value = {}
-    parent_keys_qty[parent_key] = parent_value
-    n += 1
-
+nums = int(input("Enter the number of dictionaries you would like to merge: "))
+create_dictionaries(nums)
 merge_dictionaries(**parent_keys_qty)
