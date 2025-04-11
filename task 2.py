0ef0a602c19570f051046410d274c373ea3f19f1
Florencia Scolari <florscolari0@gmail.com>
2025-04-11 20:21:43 +0800

    while loop and validation for valid number of dict now in a function

--- task 2.py
+++ task 2.py
@@ -1,3 +1,6 @@
+def main_menu():
+    print("list of options from main menu")
+
 ### Task 2 Pseudocode Guidance
 def create_dictionaries(number_of_dict):
     """Creates number of dictionaries based on user's input with empty values as placeholders"""
@@ -14,7 +17,13 @@ def merge_dictionaries(**kwargs):
     result_dict = {}
     for dict_name, dict_value in kwargs.items():
         print(f"{'-'*10}\nLet's work now on dictionary with key: {dict_name}")
-        number_of_pairs = int(input("Enter the number of key-value pairs you would like to add: "))
+        while True:
+            try:
+                number_of_pairs = int(input("Enter the number of key-value pairs you would like to add: "))
+            except ValueError as e:
+                print(f"{e}. Please enter a number")
+            else:
+                break
         i = 0
         while i != number_of_pairs:
             print(f"{'-'*2} Key-value pair #{i+1} {'-'*2}")
@@ -28,8 +37,19 @@ def merge_dictionaries(**kwargs):
     print(f"{'-'*10}\nYou have merged {len(kwargs.items())} dictionaries into 1.\nThe result has {len(result_dict.items())} key-value pairs.\n{result_dict}")
 
 
+def valid_number_of_dict():
+    while True:
+        try:
+            nums = int(input("Enter the number of dictionaries you would like to merge: "))
+            if nums <= 1:
+                print(f"{nums} is not a valid number. Please enter a number higher than 1 to merge dictionaries.")
+                continue
+            return nums
+        except ValueError as e:
+            print(f"{e}. Please enter a number")
+
 ## Running Program with User Inputs
+nums = valid_number_of_dict()
 parent_keys_qty = {}
-nums = int(input("Enter the number of dictionaries you would like to merge: "))
 create_dictionaries(nums)
 merge_dictionaries(**parent_keys_qty)
