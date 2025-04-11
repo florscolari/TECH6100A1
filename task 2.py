ff30c61aedae6e2bbfbc924bf13633611a418285
Florencia Scolari <florscolari0@gmail.com>
2025-04-11 19:03:05 +0800

    added Task 2 on task 2

--- task 2.py
+++ task 2.py
@@ -1,33 +1,30 @@
 ### Task 2 Pseudocode Guidance
-
-
-
 def merge_dictionaries(**kwargs):
     result_dict = {}
     for dict_name, dict_value in kwargs.items():
-        print(f"You are updating dictionary: {dict_name}")
-        number_of_pairs = int(input("How many key-value pairs do you want to add to this dictionary? "))
+        print(f"{'-'*10}\nLet's work now on dictionary with key: {dict_name}")
+        number_of_pairs = int(input("Enter the number of key-value pairs you would like to add: "))
         i = 0
         while i != number_of_pairs:
-            key = input("Key: ")
-            value = input("Value: ")
+            print(f"{'-'*2} Key-value pair #{i+1} {'-'*2}")
+            key = input(f"Enter the key: ")
+            value = input(f"Enter the value: ")
             dict_value[key] = value
             i += 1
         for key, value in dict_value.items():
             result_dict[key] = value
 
-    print(result_dict)
+    print(f"{'-'*10}\nYou have merged {len(kwargs.items())} dictionaries into 1.\nThe result has {len(result_dict.items())} key-value pairs.\n{result_dict}")
 
 
 
-keys_qty = {}
-number_of_dict = int(input("Number of dictionaries "))
-i = 0
-while i != number_of_dict:
-    key = input("Key: ")
-    value = {}
-    keys_qty[key] = value
-    i += 1
-print(keys_qty)
+parent_keys_qty = {}
+number_of_dict = int(input("Enter the number of dictionaries you would like to merge: "))
+n = 0
+while n != number_of_dict:
+    parent_key = input(f"Enter the Key for dictionary #{n+1}: ")
+    parent_value = {}
+    parent_keys_qty[parent_key] = parent_value
+    n += 1
 
-merge_dictionaries(**keys_qty)
+merge_dictionaries(**parent_keys_qty)
