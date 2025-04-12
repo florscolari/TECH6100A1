b1175ff727a79cbc3aa10de5fbcc630dc7783d25
Florencia Scolari <florscolari0@gmail.com>
2025-04-12 15:35:55 +0800

    task 3 is running completely on sandbox

--- sandbox.py
+++ sandbox.py
@@ -33,11 +33,14 @@ def value_for_placeholder(num):
         n += 1
     return list_of_value
 
+def replace_placeholder_for_values(template, *args):
+    formatted_text = template.format(*args)
+    return formatted_text.capitalize()
+
 ## Running Program with User Inputs
 placeholder_count = valid_number_of_placeholder() #Collects number of placeholders (function returns [0] placeholders, [1] template as string)
-template = placeholder_count[1] # This variable stores the template as s string.
-print(placeholder_count)
+template_text = placeholder_count[1] # This variable stores the template as s string.
 values = value_for_placeholder(placeholder_count[0]) #This variable stores the list of values (to be used as *args)
-
-print(values)
-print(template)
+output = replace_placeholder_for_values(template_text, *values) #passing template text & values to replace placeholder by using format method
+print(divider)
+print(f"You have entered:\nTemplate: {template_text}\nValues: {values}\nResult: {output}")
